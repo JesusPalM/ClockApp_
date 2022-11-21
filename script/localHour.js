6118c001acd45a0759437a83264b7eb44ea1887c
Jesus Palomino <palm.jes20@gmail.com>
2022-11-20 22:27:05 -0500

    Updated toggle visibility in the menu

--- script/localHour.js
+++ script/localHour.js
@@ -1,30 +1,11 @@
-const hour = document.querySelector("[data-local-hour]")
-const currentHour = moment().format('h:mm:ss');
-const AMPM = document.querySelector("[data-local-AMPM]")
-const currentAMPM = moment().format('A');
+const refreshHour = () => {
+    const hour = document.querySelector("[data-local-hour]")
+    const currentHour = moment().format('h:mm');
+    const amPm = document.querySelector("[data-local-amPm]")
+    const currentAMPM = moment().format('A');
 
-const timeRefresher = (timeDisplayed, timeVariable) => {
-    timeDisplayed.innerHTML = timeVariable;
-    return timeDisplayed;
-};
-
-setInterval(() => { 
-    timeRefresher(hour, currentHour);
-   }, 1000);
-
-/*
-const localHour = () => {
-    const hour= document.querySelector("[data-local-hour]")
-    const currentHour = moment().format('h:mm:ss');
+    amPm.innerHTML = currentAMPM;
     hour.innerHTML = currentHour;
 }
 
-const localAMPM = () =>{
-    const AMPM= document.querySelector("[data-local-AMPM]")
-    const currentAMPM = moment().format('A');
-    AMPM.innerHTML = currentAMPM;
-}
-
-setInterval(localHour,1000);
-setInterval(localAMPM,1000);
-*/
+setInterval(refreshHour,1000);
---
+++ script/menu.js
@@ -0,0 +1,26 @@
+const slideInDisplay = (element) =>{
+    element.style.animation = "slideInDisplay 2s";
+    element.style.display = "flex";
+    display = true;
+}
+
+const slideOutDisplay = (element) =>{
+    element.style.animation = "slideOutDisplay 2s";
+    setTimeout(function(){element.style.display = "git "},2000);
+}
+
+const mainMenuButton = document.querySelector("[data-main-menu-btn]");
+const mainMenu = document.querySelector("[data-main-menu]");
+
+mainMenuButton.addEventListener("click",()=>{
+    const displayStatus = mainMenu.getAttribute("data-visible");
+
+    if(displayStatus == "false"){
+        mainMenu.setAttribute("data-visible",true);
+        slideInDisplay(mainMenu);
+    }
+    else if(displayStatus == "true"){
+        mainMenu.setAttribute("data-visible",false);
+        slideOutDisplay(mainMenu);
+    }
+});
