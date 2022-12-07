f99504ece455e610d1906a33f8ea4eb8f84765c7
Jesus Palomino <palm.jes20@gmail.com>
2022-12-07 10:26:05 -0500

    Fixed menu visibility when mobile was rotated

--- script/menu.js
+++ script/menu.js
@@ -1,4 +1,16 @@
-//Slide animation display
+//Display function and animation
+
+const displaySettings = (object)=>{
+    let displayStatus = object.getAttribute("data-visible");
+    if(displayStatus === "false"){
+        object.setAttribute("data-visible",true);
+        object.style.display = "flex";
+    }
+    else{
+        object.setAttribute("data-visible",false);
+        object.style.display = "none"; 
+    }
+};
 
 const slideInDisplay = (element)=>{
     element.style.animation = "slideInDisplay 1s";
@@ -15,18 +27,26 @@ const slideOutDisplay = (element)=>{
 const mainMenuButton = document.querySelector("[data-main-menu-btn]");
 const mainMenu = document.querySelector("[data-main-menu]");
 
-const mainMenuDesktop = () => {
-    if(window.innerWidth >= 480){
+const menuDesktopDisplay = () => {
+    let mainMenuVisibility = mainMenu.getAttribute("data-visible");
+    while(window.innerWidth > 480){
+        mainMenu.setAttribute("data-visible", true);
         mainMenu.style.animation = "none";
         mainMenu.style.display = "flex";
-    }else{};
+        break;
+    }
+    while(window.innerWidth <= 480 && mainMenuVisibility === "true"){
+        mainMenu.style.display = "flex";
+        mainMenuButton.style.backgroundImage="url(../../assets/img/icons/menu/x.svg)"
+        break;
+    }
 };
 
-setInterval(mainMenuDesktop,50);
+setInterval(menuDesktopDisplay,100);
 
 mainMenuButton.addEventListener("click",()=>{
-    let displayStatus = mainMenu.getAttribute("data-visible");
-    if(displayStatus === "false"){
+    let mainMenuVisibility = mainMenu.getAttribute("data-visible");
+    if(mainMenuVisibility === "false"){
         mainMenu.setAttribute("data-visible",true);
         slideInDisplay(mainMenu);
         mainMenuButton.style.backgroundImage="url(../../assets/img/icons/menu/x.svg)"
@@ -42,21 +62,8 @@ mainMenuButton.addEventListener("click",()=>{
 
 const settingsButton = document.querySelector("[data-settings-btn]");
 const setingsMenu = document.querySelector("[data-settings]");
-
 const themeButton = document.querySelector("[data-theme-btn]");
 const themeMenu = document.querySelector("[data-theme]");
-
-function displaySettings (object){
-    let displayStatus = object.getAttribute("data-visible");
-    if(displayStatus === "false"){
-        object.setAttribute("data-visible",true);
-        object.style.display = "flex";
-    }
-    else{
-        object.setAttribute("data-visible",false);
-        object.style.display = "none"; 
-    }
-};
 
 settingsButton.addEventListener("click",()=>{displaySettings(setingsMenu);});
 themeButton.addEventListener("click",()=>{displaySettings(themeMenu);});
