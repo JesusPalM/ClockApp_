8f950dae73ce0cb8d150f887547e0cab70bfba9f
Jesus Palomino <palm.jes20@gmail.com>
2022-12-07 23:33:29 -0500

    Fixed theme toggler and checkboxes

--- script/theme.js
+++ script/theme.js
@@ -1,34 +1,28 @@
-const toggleDefaultMode = document.querySelector("[data-theme-default]");
-const toggleDarkMode = document.querySelector("[data-theme-dark]");
-const toggleLightMode = document.querySelector("[data-theme-light]");
-const toggleEarthMode = document.querySelector("[data-theme-earth]");
-
 const body = document.body;
+const html = document.querySelector("html");
+
+const defaultMode= document.querySelector("[data-theme-default]");
+const darkMode = document.querySelector("[data-theme-dark]");
+const lightMode = document.querySelector("[data-theme-light]");
+const earthMode = document.querySelector("[data-theme-earth]");
+
+const checkboxToggle = (checkbox) =>{
+    const checkboxes = document.getElementsByClassName("navigation_checkbox");
+    for (let i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = false;
+    }
+    checkbox.checked = true;
+};
 
 const themeToggle = () =>{
     body.style.animation = "displayBackground 120s linear infinite alternate";
-    if(toggleDarkMode.checked){
-        toggleLightMode.checked = false;
-        toggleEarthMode.checked = false;
+    if(darkMode.checked){
         body.style.backgroundImage = "url(../../assets/img/img/dark_bg.jpg)";
-        toggleDefaultMode.style.backgroundImage = "none"; 
-        toggleDefaultMode.style.backgroundColor = "var( --black-5)"; 
-    }else if(toggleLightMode.checked){
-        toggleDarkMode.checked = false;
-        toggleEarthMode.checked = false;
+    }else if(lightMode.checked){
         body.style.backgroundImage = "url(../../assets/img/img/light_bg.jpg)";
-        toggleDefaultMode.style.backgroundImage = "none"; 
-        toggleDefaultMode.style.backgroundColor = "var( --black-5)"; 
-    }else if(toggleEarthMode.checked){
-        toggleDarkMode.checked = false;
-        toggleLightMode.checked = false;
+    }else if(earthMode.checked){
         body.style.backgroundImage = "url(../../assets/img/img/earth_bg.jpg)";
-        toggleDefaultMode.style.backgroundImage = "none"; 
-        toggleDefaultMode.style.backgroundColor = "var( --black-5)"; 
-    }
-    else{
-        toggleDefaultMode.style.backgroundImage = "url(../../assets/img/icons/menu/check-lg.svg)";
-        toggleDefaultMode.style.backgroundColor = "var( --black-3)"; 
+    }else{
         body.style.backgroundImage = "none";
         body.style.animation = "none";
     }
